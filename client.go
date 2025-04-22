97e7d1fc9e2d8a7f56436cd9be437fba9dd733af
product-team <product.team@groupdocs.com>
2025-04-22 00:56:54 -0500

    Updated sources

--- client.go
+++ client.go
@@ -36,7 +36,7 @@ var (
 	xmlCheck  = regexp.MustCompile("(?i:(?:application|text)/xml)")
 )
 
-// APIClient manages communication with the GroupDocs.Conversion Cloud API Reference API v25.3
+// APIClient manages communication with the GroupDocs.Conversion Cloud API Reference API v25.4
 // In most cases there should be only one, shared, APIClient.
 type APIClient struct {
 	cfg    *Configuration
